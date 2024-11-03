72fd612e5f1294f8bc66ac48b550968d2f00837a
aryo <143342170+aryodw@users.noreply.github.com>
2024-11-03 18:33:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     const tosButton = document.getElementById('tos-button');
-    const modal = document.getElementById('modal');
+    const modal = document.getElementById('tos-modal');
     const closeButton = document.querySelector('.close-button');
 
     tosButton.addEventListener('click', (event) => {
-        event.preventDefault();
+        event.preventDefault(); // Zapobiega domyślnej akcji linku
         modal.style.display = 'block';
-        document.body.style.overflow = 'hidden'; // Disable scrolling
     });
 
     closeButton.addEventListener('click', () => {
         modal.style.display = 'none';
-        document.body.style.overflow = 'auto'; // Re-enable scrolling
     });
 
     window.addEventListener('click', (event) => {
-        if (event.target == modal) {
+        if (event.target === modal) {
             modal.style.display = 'none';
-            document.body.style.overflow = 'auto'; // Re-enable scrolling
         }
     });
 });
